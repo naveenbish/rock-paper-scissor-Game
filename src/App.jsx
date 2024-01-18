3f67aeeb398a3f04aef938cc29646b5b9b3ab1fe
navbis <naveenbish>
2024-01-18 15:47:01 +0530

    Fontend changes made for Destop View

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Challenges from './components/Challenges'
+import Playground from './components/Playground'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex'>
+      {/* Challenger updates the Challeneg Left-side of Game */}
+      <div className='w-[40%]'><Challenges></Challenges></div>
+
+      {/* verticle middle divider  */}
+      <div className='border border-black h-[100vh]'></div>
+
+      {/* The playground Where the result and animation will take place Right-side */}
+      <div className='w-[60%]'>
+        <Playground></Playground>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
